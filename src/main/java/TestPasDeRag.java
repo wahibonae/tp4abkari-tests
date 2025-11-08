1bf72ed35be0de2194dca872c364ff377afe0a51
wahibonae <wahibabkari@gmail.com>
2025-11-08 22:33:58 +0100

    Finished Test 4

--- src/main/java/TestPasDeRag.java
+++ src/main/java/TestPasDeRag.java
@@ -1,5 +1,34 @@
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.rag.query.Query;
+import dev.langchain4j.rag.query.router.QueryRouter;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
 
 public class TestPasDeRag {
     public static void main(String[] args) throws Exception {
@@ -9,7 +38,88 @@ public class TestPasDeRag {
                 .builder()
                 .apiKey(cle)
                 .modelName("gemini-2.0-flash-exp")
-                .temperature(0.6)
+                .temperature(0.3)
+                .logRequestsAndResponses(true)
                 .build();
+
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+
+        URL fileUrl = TestRoutage.class.getResource("rag.pdf");
+        Path path = Paths.get(fileUrl.toURI());
+
+        DocumentParser documentParser = new ApacheTikaDocumentParser();
+
+        Document document = FileSystemDocumentLoader.loadDocument(path, documentParser);
+
+        DocumentSplitter splitter = DocumentSplitters.recursive(300, 50);
+        List<TextSegment> segments = splitter.split(document);
+
+        List<Embedding> embeddings = embeddingModel.embedAll(segments).content();
+
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        embeddingStore.addAll(embeddings, segments);
+
+        ContentRetriever retriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .maxResults(3)
+                .minScore(0.5)
+                .build();
+
+        // Notre QueryRouter personnalisé
+        QueryRouter queryRouter = new QueryRouter() {
+            @Override
+            public Collection<ContentRetriever> route(Query query) {
+                PromptTemplate promptTemplate = PromptTemplate.from(
+                        "Est-ce que la requête `{{requete}}` porte sur l'IA? Réponds seulement par 'oui', 'non' ou 'peut-être'."
+                );
+
+                Prompt prompt = promptTemplate.apply(Map.of(
+                        "requete", query.text()
+                ));
+
+                String reponse = model.chat(prompt.text());
+
+                if (reponse.toLowerCase().contains("non")) {
+                    return Collections.emptyList();
+                }
+
+                return Collections.singletonList(retriever);
+            }
+        };
+
+        RetrievalAugmentor retrievalAugmentor = DefaultRetrievalAugmentor.builder()
+                .queryRouter(queryRouter)
+                .build();
+
+        ChatMemory memory = MessageWindowChatMemory.withMaxMessages(10);
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatModel(model)
+                .retrievalAugmentor(retrievalAugmentor)
+                .chatMemory(memory)
+                .build();
+
+        conversationAvec(assistant);
+    }
+
+    private static void conversationAvec(Assistant assistant) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
     }
 }
