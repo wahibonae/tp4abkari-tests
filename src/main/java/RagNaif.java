3f0eebe95c5d74dcf443dbfd6bdb2cfae837483f
wahibonae <wahibabkari@gmail.com>
2025-11-08 20:25:35 +0100

    Finished Test 1

---
+++ src/main/java/RagNaif.java
@@ -0,0 +1,95 @@
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class RagNaif {
+    public static void main(String[] args) throws URISyntaxException {
+        String cle = System.getenv("GEMINI_KEY");
+
+        ChatModel model = GoogleAiGeminiChatModel
+                .builder()
+                .apiKey(cle)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.6)
+                .build();
+
+        // Phase 1
+        URL fileUrl = RagNaif.class.getResource("rag.pdf");
+        Path path = Paths.get(fileUrl.toURI());
+
+        DocumentParser documentParser = new ApacheTikaDocumentParser();
+
+        Document document = FileSystemDocumentLoader.loadDocument(path, documentParser);
+
+        DocumentSplitter splitter = DocumentSplitters.recursive(300, 50);
+        List<TextSegment> segments = splitter.split(document);
+
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+
+        List<Embedding> embeddings = embeddingModel.embedAll(segments).content();
+
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        embeddingStore.addAll(embeddings, segments);
+
+        // Phase 2
+        ContentRetriever retriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingModel(embeddingModel)
+                .embeddingStore(embeddingStore)
+                .maxResults(4)
+                .minScore(0.5)
+                .build();
+
+        ChatMemory memory = MessageWindowChatMemory.withMaxMessages(10);
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatModel(model)
+                .contentRetriever(retriever)
+                .chatMemory(memory)
+                .build();
+
+        conversationAvec(assistant);
+    }
+
+    private static void conversationAvec(Assistant assistant) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
+    }
+}
